bb821458a519d100f70e37bf4c28495337fa6ad9
Nizobe <nbessi2@uic.edu>
2015-04-23 15:40:07 -0500

    Added times to locations file

--- data/locations.js
+++ data/locations.js
@@ -3,18 +3,26 @@ var niko_locations = {
 	"geometry": {
 		"type": "MultiPoint",
 		"coordinates": [
-			[47.606284, -122.331786],
-			[21.306479, -157.855962],
-			[32.757515, -97.336011],
-			[32.528217, -93.757405],
-			[42.110741, -88.032199],
-			[41.866114, -87.646157],
-			[41.874008, -87.686129]
+			[47.606284, -122.331786],//seattle
+			[21.306479, -157.855962],//honolulu
+			[32.757515, -97.336011],//fort worth
+			[32.528217, -93.757405],//shreveport
+			[42.110741, -88.032199],//palatine
+			[41.866114, -87.646157],//JST
+			[41.874008, -87.686129]//harrison & western
 		]//end coords
 	},//end geometry
 	"properties": {
 		"title": "Niko",
 		"path_options": {"color": "red"},
-		
+		"time": [
+			664502400,//seattle
+			694224000,//honolulu
+			820454400,//fort worth
+			852076800,//shreveport
+			1041379200,//palatine
+			1251244800,//uic (jst)
+			1404172800,//harrison & western
+		],//end time
 	}//end properties
 }//end niko_locations
