431361a51001e539ba5f3cbf95001a20eacde170
Nizobe <nbessi2@uic.edu>
2015-04-23 15:29:25 -0500

    Created locations.js
    Currently just holds test locations. Needs time stamps still.

---
+++ data/locations.js
@@ -0,0 +1,20 @@
+var niko_locations = {
+	"type": "Feature",
+	"geometry": {
+		"type": "MultiPoint",
+		"coordinates": [
+			[47.606284, -122.331786],
+			[21.306479, -157.855962],
+			[32.757515, -97.336011],
+			[32.528217, -93.757405],
+			[42.110741, -88.032199],
+			[41.866114, -87.646157],
+			[41.874008, -87.686129]
+		]//end coords
+	},//end geometry
+	"properties": {
+		"title": "Niko",
+		"path_options": {"color": "red"},
+		
+	}//end properties
+}//end niko_locations
